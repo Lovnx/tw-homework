7216d568f642371e7123f8b12bb959a345d1b635
Lovnx <930999349@qq.com>
2018-02-12 17:17:42 +0800

    update1.0.1

--- src/test/java/com/tw/homework/conference/WholeProcessTest.java
+++ src/test/java/com/tw/homework/conference/WholeProcessTest.java
@@ -34,34 +34,34 @@ public class WholeProcessTest {
 		int t1a = 0, t1p = 0, t2a = 0, t2p = 0;
 		System.out.println("Track 1:");
 		List<Conference> trackOneAm = map.get("trackOneAm");
-		for (Conference Conference : trackOneAm) {
+		for (Conference conference : trackOneAm) {
 			Date morning = TimeUtil.getMorningBegins();
-			System.out.println(TimeUtil.getTimeAfter(morning, t1a) + Conference.getTitle());
-			t1a += Conference.getTimeDuration();
+			System.out.println(TimeUtil.getTimeAfter(morning, t1a) + conference.getTitle());
+			t1a += conference.getTimeDuration();
 		}
 		System.out.println("12:00 PM Lunch");
 		List<Conference> trackOnePm = map.get("trackOnePm");
-		for (Conference Conference : trackOnePm) {
+		for (Conference conference : trackOnePm) {
 			Date aftermoon = TimeUtil.getAfternoonBegins();
-			System.out.println(TimeUtil.getTimeAfter(aftermoon, t1p) + Conference.getTitle());
-			t1p += Conference.getTimeDuration();
+			System.out.println(TimeUtil.getTimeAfter(aftermoon, t1p) + conference.getTitle());
+			t1p += conference.getTimeDuration();
 		}
 		
 		System.out.println();
 		
 		System.out.println("Track 2:");
 		List<Conference> trackTwoAm = map.get("trackTwoAm");
-		for (Conference Conference : trackTwoAm) {
+		for (Conference conference : trackTwoAm) {
 			Date morning = TimeUtil.getMorningBegins();
-			System.out.println(TimeUtil.getTimeAfter(morning, t2a) + Conference.getTitle());
-			t2a += Conference.getTimeDuration();
+			System.out.println(TimeUtil.getTimeAfter(morning, t2a) + conference.getTitle());
+			t2a += conference.getTimeDuration();
 		}
 		System.out.println("12:00 PM Lunch");
 		List<Conference> trackTwoPm = map.get("trackTwoPm");
-		for (Conference Conference : trackTwoPm) {
+		for (Conference conference : trackTwoPm) {
 			Date aftermoon = TimeUtil.getAfternoonBegins();
-			System.out.println(TimeUtil.getTimeAfter(aftermoon, t2p) + Conference.getTitle());
-			t2p += Conference.getTimeDuration();
+			System.out.println(TimeUtil.getTimeAfter(aftermoon, t2p) + conference.getTitle());
+			t2p += conference.getTimeDuration();
 		}
 		System.out.println();
 		System.out.println("------------------spend total: "+(System.currentTimeMillis() - begin)+"ms------------------");
